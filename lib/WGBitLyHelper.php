e33d98bad30d93888965fb27b24250f75905d46b
webbgaraget <webbgaraget@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-02 07:43:36 +0000

    Tagging version 0.3
    git-svn-id: https://plugins.svn.wordpress.org/wg-bitly-shortener/trunk@538815 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/WGBitLyHelper.php
+++ lib/WGBitLyHelper.php
@@ -14,17 +14,60 @@ class WGBitlyHelper
         $pro_domain = get_option( 'wg-bitly-pro-domain' );
         
         return $domain == -1 ? $pro_domain : $domain;
-  }
+	}
   
-  /**
-   * Check if we're using a pro domain.
-   *
-   * @return void
-   * @author Erik Hedberg (erik@webbgaraget.se)
-   */
-  static function using_pro_domain()
-  {
-      return ( get_option( 'wg-bitly-domain' ) == -1 );
-  }
+	/**
+	 * Check if we're using a pro domain.
+	 *
+	 * @return void
+	 * @author Erik Hedberg (erik@webbgaraget.se)
+	 */
+	static function using_pro_domain()
+	{
+	    return ( get_option( 'wg-bitly-domain' ) == -1 );
+	}
+
+	/**
+	 * Get the shortened link
+	 *
+	 * @param string $post_id 
+	 * @return void
+	 * @author Erik Hedberg (erik@webbgaraget.se)
+	 */
+	static function get_short_link( $post_id )
+	{
+		if ( !$post_id )
+		{
+			global $post;
+			$post_id = $post->ID;
+		}
+		$hash = unserialize( get_post_meta( $post_id, 'wg-bitly-hash', true) );
+		if ( $hash->short_url )
+		{
+			return $hash->short_url;
+		}
+		return null;
+	}
+	
+	/**
+	 * Outputs a link with the shortened URL
+	 *
+	 * @param string $post_id 
+	 * @return void
+	 * @author Erik Hedberg (erik@webbgaraget.se)
+	 */
+	static function the_short_link( $post_id = null )
+	{
+		if ( !$post_id )
+		{
+			global $post;
+			$post_id = $post->ID;
+		}
+		$short = self::get_short_link( $post_id );
+		?>
+		<?php if ( $short ): ?>
+			<a href="<?php echo $short; ?>"><?php echo $short ?></a>
+		<?php endif; ?>
+		<?php
+	}
 }
-?>
